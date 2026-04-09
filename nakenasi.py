dd4e7af28e22de8a171424b54fde01e1a6364ba6
E.Watai <113270697+RAI09522@users.noreply.github.com>
2026-04-09 23:19:59 +0000

    ファイルからの知識の読み込み

--- nakenasi.py
+++ nakenasi.py
@@ -5,3 +5,7 @@ def load_brain(file_path):
         with open(file_path, "r", ecoding="utf-8") as f:
             for line in f:
                 line = line.strip()
+                if "|" in line:
+                    key, val = line.split("|", 1)
+                    brain_data[key] = val
+    return brain_data              
